152e626c16d4270efca512776fff81c0517e1183
Ahnaf Bin Zakaria <141830611+AhnafZ778@users.noreply.github.com>
2025-06-15 20:20:36 +0600

    Main Body Code

---
+++ Pokemon.py
@@ -0,0 +1,39 @@
+from Pokemon_data.Move_lister import Get_Pokemon_info
+from Pokemon_data.Move_lister import move_retreiver
+from Pokemon_data.Ability_lister import ability_list
+from Pokemon_data.Stats_lister import stat_lister
+from Pokemon_data.Type_lister import type_lister
+
+class Pokemon:
+    def __init__(self, Name):
+        self.name = Name
+        self.Pokemon_info = Get_Pokemon_info(self.name)
+    def separate(self):
+        self.Abilities = open("Pokemon_data/Pokemon abilities.py", "a", encoding= "UTF-8")
+        self.Types = open("Pokemon_data/Pokemon Types.py", "a", encoding= "UTF-8")
+        self.Stats = open("Pokemon_data/Pokemon Stats.py", "a", encoding= "UTF-8")
+        for i in self.Pokemon_info:
+            if i == "abilities":
+                self.Abilities.write(f"{self.Pokemon_info[i]}")
+                self.Abilities.write("\n")
+            elif i == "moves":
+                self.Moves.write(f"{self.Pokemon_info[i]}")
+                self.Moves.write("\n")
+            elif i == "types":
+                self.Types.write(f"{self.Pokemon_info[i]}")
+                self.Types.write("\n")
+            elif i == "stats":
+                self.Stats.write(f"{self.Pokemon_info[i]}")
+                self.Stats.write("\n")
+    def retriever(self):
+        return move_retreiver(self.name)
+
+    
+Pokemon_Name = input("Insert the name of your desired pokemon: \n")
+if Get_Pokemon_info(Pokemon_Name):
+    Pokemon_Name = Pokemon(Pokemon_Name)
+moveset, moves = Pokemon_Name.retriever()
+Ability, status = ability_list(Pokemon_Name.Pokemon_info)
+Stats = stat_lister(Pokemon_Name.Pokemon_info)
+Typing = type_lister(Pokemon_Name.Pokemon_info)
+print(Ability, Stats, Typing)
