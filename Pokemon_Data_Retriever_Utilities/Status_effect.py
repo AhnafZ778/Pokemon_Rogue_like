15aca775cf3da400168390876bb21fda88485611
Ahnaf Bin Zakaria <141830611+AhnafZ778@users.noreply.github.com>
2025-06-22 07:27:06 +0600

    Create Status_effect.py

---
+++ Pokemon_Data_Retriever_Utilities/Status_effect.py
@@ -0,0 +1,13 @@
+def Status_effect(effect):
+    if effect == "paralysis":
+        print("Congrats you're practically Stephen Hawkins now")
+        return "Paralyzed"
+    elif effect == "burn":
+        print("Bro is the Human Torch now")
+        return "Burned"
+    elif effect == "freeze":
+        print("LET IT MOTHERFUCKING GO AHH SPELL")
+        return "Frozen"
+    elif effect == "poison":
+        print("Pfft someone nom nommed by a python")
+        return "Poisoned"
