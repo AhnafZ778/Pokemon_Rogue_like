6e9d39c8760142523d77c7e421e7cb55b9d86ca4
Ahnaf Bin Zakaria <141830611+AhnafZ778@users.noreply.github.com>
2025-06-22 16:23:54 +0600

    Patched it up a lil bit

--- Pokemon_Data_Retriever_Utilities/Status_effect.py
+++ Pokemon_Data_Retriever_Utilities/Status_effect.py
@@ -1,13 +1,17 @@
-def Status_effect(effect): 
+def Status_effect(effect, Pokemon = None):
     if effect == "paralysis":
         print("Congrats you're practically Stephen Hawkins now")
         return "Paralyzed"
     elif effect == "burn":
         print("Bro is the Human Torch now")
+        Pokemon.Stats["attack"] /= 2
         return "Burned"
     elif effect == "freeze":
         print("LET IT MOTHERFUCKING GO AHH SPELL")
         return "Frozen"
     elif effect == "poison":
-        print("Pfft someone nom nommed by a python")
+        print("Pfft someone got nom nommed by a python")
         return "Poisoned"
+    elif effect == "sleep":
+        print("Bro fell asleep at the job")
+        return "Sleep"
