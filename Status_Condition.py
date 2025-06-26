713666232ee9dee9f0ebe449372519d1e25508f2
Ahnaf Bin Zakaria <141830611+AhnafZ778@users.noreply.github.com>
2025-06-26 13:53:02 +0600

    Fixed the return values and uses of those

--- Status_Condition.py
+++ Status_Condition.py
@@ -4,11 +4,14 @@ def Status_condition_effect(Pokemon):
     if Pokemon.Status_condition == "Paralyzed":
         dice = random.randint(1, 100)
         if dice <= 25:
-            print("You got Stephen Hawkins'ed")
-            return True
+            if Pokemon.Status_condition:
+                print("Bro is already disabled dude chill")
+                return True
+            else:
+                print("You got Stephen Hawkins'ed")
+                return True
         else:
             return False
-    
     elif Pokemon.Status_condition == "Poisoned":
         damage = (1/8)*(Pokemon.Stats["hp"])
         Pokemon.HP -= damage
@@ -16,22 +19,32 @@ def Status_condition_effect(Pokemon):
     elif Pokemon.Status_condition == "Frozen":
         dice = random.randint(1, 100)
         if dice <= 45:
-            print("You're Still frozen my guy")
-            return True
+            if Pokemon.Status_condition:
+                print("Bro is already disabled dude chill")
+                return True
+            else:
+                print("You're Still frozen my guy")
+                return False
         else:
-            print("Congrats you made elsa your bitch")
+            print("Congrats you have officially let it go")
             Pokemon.Status_condition = None
-            return False
+            return True
+        
     elif Pokemon.Status_condition == "Burned":
         damage = (1/16)*(Pokemon.Stats["hp"])
         Pokemon.HP -= damage
         print(f"{Pokemon.name} took {damage} damage from Burn")
-
+        
+        
     elif Pokemon.Status_condition == "Sleep":
         dice = random.randint(1, 100)
         if dice <= 45:
-            print("Wake up you fucking sloth")
-            return True
+            if Pokemon.Status_condition:
+                print("Bro is already disabled dude chill")
+                return True
+            else:
+                print("Wake up you fucking sloth")
+                return False
         else:
             print("Congrats you're no longer useless")
             Pokemon.Status_condition = None
