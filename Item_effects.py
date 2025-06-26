58513ee280596587b1b8247460acaac0ecccf6c2
Ahnaf Bin Zakaria <141830611+AhnafZ778@users.noreply.github.com>
2025-06-26 13:51:44 +0600

    Added Awakening also capitalized the print statements

--- Item_effects.py
+++ Item_effects.py
@@ -1,4 +1,5 @@
 def effect(pokemon, item, trainer):
+    print(f"{trainer.name.capitalize()} used {item} on {pokemon.name.capitalize()}")
     if item == "potion":
         pokemon.HP += 20
         if pokemon.Stats["hp"] < pokemon.HP:
@@ -6,25 +7,31 @@ def effect(pokemon, item, trainer):
     elif item == "antidote":
         if pokemon.Status_condition == "Poisoned":
             pokemon.Status_condition = None
-            print(f"{pokemon.name}'s poison got healed")
+            print(f"{pokemon.name.capitalize()}'s poison got healed")
         else:
             print("It had no effect")
     elif item == "paralyze_heal":
         if pokemon.Status_condition == "Paralyzed":
             pokemon.Status_condition = None
-            print(f"{pokemon.name} is no longer paralyzed")
+            print(f"{pokemon.name.capitalize()} is no longer paralyzed")
         else:
             print("It had no effect")
     elif item == "freeze_heal":
         if pokemon.Status_condition == "Frozen":
             pokemon.Status_condition = None
-            print(f"{pokemon.name} is no longer frozen")
+            print(f"{pokemon.name.capitalize()} is no longer frozen")
         else:
             print("It had no effect")
     elif item == "burn_heal":
         if pokemon.Status_condition == "Burned":
             pokemon.Status_condition = None
-            print(f"{pokemon.name}'s burn got healed")
+            print(f"{pokemon.name.capitalize()}'s burn got healed")
+        else:
+            print("It had no effect")
+    elif item == "awakening":
+        if pokemon.Status_condition == "Sleep":
+            pokemon.Status_condition = None
+            print(f"{pokemon.name.capitalize()} woke up")
         else:
             print("It had no effect")
     elif item == "revives":
@@ -32,7 +39,7 @@ def effect(pokemon, item, trainer):
             print("Which pokemon do you want to revive")
             count = 1
             for i in trainer.Dead_Pokemons:
-                print(f"{count}. {i.name}")
+                print(f"{count}. {i.name.capitalize()}")
                 count += 1
             response = int(input("\n"))
             response -= 1
@@ -41,6 +48,6 @@ def effect(pokemon, item, trainer):
             pokemon = trainer.Dead_Pokemons.pop(response)
             trainer.Alive_Pokemons.append(res_pokemon)
             res_pokemon.HP = res_pokemon.Stats["hp"]//2
-            print(f"{res_pokemon.name} has risen from the dead")
+            print(f"{res_pokemon.name.capitalize()} has risen from the dead")
         else:
-            print("Pokemon is already alive you dimwit")
+            print("All of your Pokemons are alive you dimwit")
