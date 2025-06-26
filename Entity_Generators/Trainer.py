9b0100e53e17117b7a8a79b1474c5c92acdca06c
Ahnaf Bin Zakaria <141830611+AhnafZ778@users.noreply.github.com>
2025-06-26 13:52:33 +0600

    Added items and also a pokemon giving generator

--- Entity_Generators/Trainer.py
+++ Entity_Generators/Trainer.py
@@ -1,3 +1,6 @@
+import uuid
+import random
+from pokemon import Pokemon
 from names_generator import generate_name
 
 class Trainer:
@@ -5,15 +8,22 @@ class Trainer:
     def __init__(self):
         random_name = generate_name()
         first, last = random_name.split("_")
-        self.trainer_name = first + " " + last
+        self.name = last.capitalize()
+        self.Alive_Pokemons = []
+        self.Dead_Pokemons = []
         self.trainer_ID = uuid.uuid1()
-        self.pokemon_ID = random.randrange(1, 50, 5)
-    def give_Pokemons(self):
-        number = random.randint(1,5)
+        self.items = {}
+        self.items["potion"] = 3
+        self.items["antidote"] = 2
+        self.items["freeze_heal"] = 0
+        self.items["paralyze_heal"] = 0
+        self.items["burn_heal"] = 0
+        
+    def give_Pokemons(self, num = 5):
+        number = random.randint(1,num)
         self.Pokemons = []
         self.Pokemons_reference = []
         for i in range(number):
             pokemon_ID = random.randint(1, 500)
             new_Pokemon = (Pokemon(pokemon_ID))
-            self.Pokemons.append(new_Pokemon.name)
-            self.Pokemons_reference.append(new_Pokemon)
+            self.Alive_Pokemons.append(new_Pokemon)
